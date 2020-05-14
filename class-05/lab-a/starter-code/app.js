b1bc067813a523a7c885c2326368ce30b2849be1
Nedal_Erekat <nedalerekat@gmail.com>
2020-05-15 00:57:09 +0300

    Pass stretch goal

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -155,10 +155,21 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+    var totalMlty;
+    var msg;
+    var arrayLength=dynamicArray.length;
+    for(var j=1;j<arrayLength;j++){
+        if(j<2){
+            totalMlty=multiply(dynamicArray[j-1],dynamicArray[j])[0];
+        }else{
+            totalMlty=multiply(totalMlty,dynamicArray[j])[0];
+        }
+    }
+    msg=`The numbers ${dynamicArray} have a product of ${totalMlty}.`
+    return[totalMlty,msg];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
